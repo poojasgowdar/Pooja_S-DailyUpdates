e87828c67de7c43caaf62dbaeb47824c69ead99c
pooja_s <166943972+poojasgowdar@users.noreply.github.com>
2024-11-20 18:25:09 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo_DLL
+namespace Lambda_Exp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>() { 2, 3, 4, 5, 6, 7, 8 };
+            //foreach(var i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            var Square = list.FindAll(x => (x%2) ==0);
+            foreach(var i in Square)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
         }
     }
 }
