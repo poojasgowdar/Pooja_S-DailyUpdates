139ac6b3ea6fa8d5d2008eff001b126cebb757dd
pooja_s <166943972+poojasgowdar@users.noreply.github.com>
2024-11-20 18:21:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoClassLibrary
+{
+    public class Class1
+    {
+        public void AreaOfTriangle()
+        {
+            int side1 = 4, side2 = 13, side3 = 15;
+            Console.WriteLine($"Value of side1 = {side1}, side2 = {side2}, and side2 = {side2}");
+            double semiperimeter = (side1 + side2 + side3) / 2;
+            double Area = Math.Sqrt(semiperimeter * (semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3));
+            Console.Write("Area of a Triangle = " + Area);
+            Console.ReadKey();
+        }
+    }
+}
