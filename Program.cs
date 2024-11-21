6c4d0179950706c0632dccf6768a766a09a73f3e
pooja_s <166943972+poojasgowdar@users.noreply.github.com>
2024-11-21 18:48:13 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
-namespace AnonymousMethod_Ex
+namespace AnonymousMethod_Ex1
 {
     internal class Program
     {
 
-        public delegate void Result(string message);
-
+        public delegate int dname(int a, int b);
         static void Main(string[] args)
         {
-            Result s = delegate (string message)
+            dname d = delegate (int a, int b)
             {
-                Console.WriteLine("Good Morning " +message);
+                //Console.WriteLine(a + b);
+                return a + b;
             };
-            s("John");
+            dname d1 = delegate (int a, int b)
+            {
+                //Console.WriteLine(a - b);
+                return a - b;
+            };
+            //d(4, 5);
+            //d1(4, 5);
+            Console.WriteLine("Addition :" + d(4, 5));
+            Console.WriteLine("Subtraction:" + d1(5, 5));
             Console.ReadKey();
         }
     }
